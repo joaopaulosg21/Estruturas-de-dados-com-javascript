1fc91da90f7db151c422e57f2682378c53ce08cf
JoãoPaulo <joaopaulosg17@gmail.com>
2022-07-27 08:59:26 -0300

    Estrutura de dados pilha usando array

---
+++ stack-array.js
@@ -0,0 +1,42 @@
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+
+    push(element) {
+        this.items.push(element);
+    }
+
+    pop() {
+        if(this.isEmpty()){
+            return "Stack is empty";
+        }
+        return this.items.pop();
+    }
+
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
+    peek() {
+        if(this.isEmpty()) {
+            return "Stack is empty";
+        }
+        return this.items[this.items.length-1];
+    }
+
+    size() {
+        return this.items.length;
+    }
+}
+
+const stack = new Stack();
+console.log(stack.isEmpty());
+stack.push(1);
+stack.push(2);
+stack.push(3);
+console.log(stack.peek());
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.size());
+console.log(stack.peek());
